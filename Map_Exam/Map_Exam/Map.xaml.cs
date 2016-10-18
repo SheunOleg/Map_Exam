17606c1761e523d576a2ac0992073521b45e893a
SheunOleg <sasski15naryto@mail.ru>
2016-10-18 20:16:19 +0300

    Добавление клиента

---
+++ Map_Exam/Map_Exam/Map.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+
+namespace Map_Exam
+{
+    /// <summary>
+    /// Логика взаимодействия для Map.xaml
+    /// </summary>
+    public partial class Map : Window
+    {
+        List<PointLatLng> Lpll = new List<PointLatLng>();
+        public Map()
+        {
+            InitializeComponent();
+        }
+
+        private void gmap_Loaded(object sender, RoutedEventArgs e)
+        {
+            gmap.MapProvider = BingMapProvider.Instance;
+            gmap.SetPositionByKeywords("Paris, France");
+        }
+
+        private void gmap_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(gmap);
+            PointLatLng pll = gmap.FromLocalToLatLng((int)p.X, (int)p.Y);
+            Lpll.Add(pll);
+            GMapMarker gmm = new GMapMarker(pll);
+            gmm.Shape = new Ellipse() { Width = 10, Height = 10, Fill = Brushes.Red };
+            gmap.Markers.Add(gmm);
+      }
+
+        //private void gmap_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    RoutingProvider rp = gmap.MapProvider as RoutingProvider;
+        //    List<PointLatLng> ps = new List<PointLatLng>();
+        //    for (int i = 0; i < Lpll.Count-1; i++)
+        //    {
+        //        MapRoute r = rp.GetRoute(Lpll[i], Lpll[i + 1], false, true, 13);
+        //        ps.AddRange(r.Points);
+        //    }
+        //    GMapRoute mapRoute = new GMapRoute(ps);
+        //    gmap.Markers.Add(mapRoute);
+        //    gmap.ZoomAndCenterMarkers(13);
+        //}
+    }
+}
