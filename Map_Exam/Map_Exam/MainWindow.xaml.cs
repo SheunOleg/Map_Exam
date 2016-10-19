10f6098ef744bf43217eb58643283087d46a6f1d
SheunOleg <sasski15naryto@mail.ru>
2016-10-19 10:54:34 +0300

    1111

--- Map_Exam/Map_Exam/MainWindow.xaml.cs
+++ Map_Exam/Map_Exam/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
 
 namespace Map_Exam
 {
@@ -20,13 +22,63 @@ namespace Map_Exam
     /// </summary>
     public partial class MainWindow : Window
     {
+        OracleConnection oracleConn { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            oracleConn = new OracleConnection("Data Source=10.3.0.227;User Id=Sheun;Password=1111;");
+            try
+            {
+                oracleConn.Open();
+            }
+            catch (Exception e)
+            {
+                oracleConn.Close();
+                Close();
+            }
         }
 
         private void Button_Click_Login(object sender, RoutedEventArgs e)
         {
+            OracleCommand com = new OracleCommand();
+            com.Connection = oracleConn;
+
+            com.CommandText = "BEGIN PROVER(':login',':password')";
+            com.Parameters.Add("login", OracleDbType.NVarchar2, 10).Value = Login.Text;
+            com.Parameters.Add("password", OracleDbType.NVarchar2, 10).Value = Password.Text;
+            OracleDataReader res = com.ExecuteReader();
+            while (res.Read())
+            {
+                if ((bool)res[0] == true)
+                { 
+                }
+                else 
+                { 
+                }
+            }
+        }
+
+        private void Button_Click_Registr(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                OracleCommand com = new OracleCommand("Select * From OPERATOR_;", oracleConn);
+                //OracleCommand com = new OracleCommand("SELECT * FROM ALL_TABLES", oracleConn);
+                //com.CommandText = "INSERT INTO OPERATOR_(ID,NAME,PSWD) VALUES(2,':login',':password');";
+                //com.Parameters.Add("login", OracleDbType.NVarchar2, 10).Value = Login.Text;
+                //com.Parameters.Add("password", OracleDbType.NVarchar2, 10).Value = Password.Text;
+                OracleDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    Console.WriteLine("\t{0}\t{1}\t{2}",
+                    reader[0], reader[1], reader[2]);
+                }
+                reader.Close();
+                
+            }
+            catch { }
 
         }
     }
